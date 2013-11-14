90297d4f031f655f12ba448b8bf471cebd624c3b
macmule <bentoms@btopenworld.com>
2013-11-14 21:46:57 +0000

    Update EAgetSilverlightPluginVersion.sh

--- EAgetSilverlightPluginVersion.sh
+++ EAgetSilverlightPluginVersion.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+####################################################################################################
+#
+# More information: http://macmule.com/2011/03/07/ea-get-silverlight-version/
+#
+# GitRepo: https://github.com/macmule/getSilverlightPluginVersion/
+#
+# License: http://macmule.com/license/
+#
+####################################################################################################
 
 # Checks to see if the plugin exists
 if [ -e /Library/Internet\ Plug-Ins/Silverlight.plugin ]; then
